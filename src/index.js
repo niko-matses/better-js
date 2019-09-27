e0a3f98a18e977ef32035cee79651ca277a1b496
niko-matses <nikolas.matses@gmail.com>
2019-09-27 16:38:01 -0400

    interval and setTimeout on color changing bg

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+import { timeout } from "q";
+
 // Generate random color
 // // RGB 0-255
 // Apply to dom
 // update color on event
-
 const generateColorValue = () => Math.floor(Math.random() * 256);
 
 const createColor = () => {
@@ -20,13 +21,15 @@ const addRandomColorToBg = () => {
   const color = createColor();
   return applyColorToBody(color);
 };
-
 addRandomColorToBg();
 
 const newColors = document.getElementById("new-colors");
-// On click attach 1 event
-// newColors.onclick = () => addRandomColorToBg();
-// newColors.onclick = () => console.log("hi");
 
 newColors.addEventListener("click", addRandomColorToBg);
-// newColors.addEventListener("click", () => console.log("hi"));
+
+// Set interval vs Set Timeout
+// const log = () => console.log('is in timeout');
+// setTimeout(log, 1000);
+
+const interval = setInterval(addRandomColorToBg, 2000);
+newColors.addEventListener("click", () => clearInterval(interval));
