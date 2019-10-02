e5a83024e310a3114fcc393a678d59485213cd7d
niko-matses <nikolas.matses@gmail.com>
2019-10-02 16:20:04 -0400

    async await overview

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -118,18 +118,16 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"src/index.js":[function(require,module,exports) {
-// Callback Hell:
-// chargeCard('12341234123', (res) => {
-//   // time to do something
-//   saveToDb(res, () => {
-//     // time to do something else
-//   })
-// });
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+// Made up code
+// imported chargeCard
 var chargeCard = function chargeCard() {
   return new Promise(function (resolve, reject) {
     // card is valid
-    reject("Fail!!! :(");
-
+    // reject("fail!!!");
     if (true) {
       return resolve(true);
     }
@@ -138,11 +136,49 @@ var chargeCard = function chargeCard() {
   });
 };
 
-chargeCard().then(function (val) {
-  console.log(val);
-}).catch(function (err) {
-  console.log(err);
-});
+var chargeCC =
+/*#__PURE__*/
+function () {
+  var _ref = _asyncToGenerator(
+  /*#__PURE__*/
+  regeneratorRuntime.mark(function _callee(ccNumber) {
+    var res;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.prev = 0;
+            _context.next = 3;
+            return chargeCard(ccNumber);
+
+          case 3:
+            res = _context.sent;
+            console.log(res);
+            return _context.abrupt("return", res);
+
+          case 8:
+            _context.prev = 8;
+            _context.t0 = _context["catch"](0);
+            console.log(_context.t0);
+
+          case 11:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee, null, [[0, 8]]);
+  }));
+
+  return function chargeCC(_x) {
+    return _ref.apply(this, arguments);
+  };
+}(); // Callback hell
+// chargeCard('12341234123', (res) => {
+//   // time to do something
+//   saveToDb(res, () => {
+//     // time to do something else
+//   })
+// });
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -171,7 +207,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49880" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58519" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
