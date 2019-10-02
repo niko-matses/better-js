fc47d2cad861e54b111ecd052dd251b5e28296f3
niko-matses <nikolas.matses@gmail.com>
2019-10-02 16:03:12 -0400

    promises concept using then and catch

--- src/index.js
+++ src/index.js
@@ -1,35 +1,24 @@
-import { timeout } from "q";
+// Callback Hell:
+// chargeCard('12341234123', (res) => {
+//   // time to do something
+//   saveToDb(res, () => {
+//     // time to do something else
+//   })
+// });
 
-// Generate random color
-// // RGB 0-255
-// Apply to dom
-// update color on event
-const generateColorValue = () => Math.floor(Math.random() * 256);
+const chargeCard = () => new Promise((resolve, reject) => {
+  // card is valid
+  reject("Fail!!! :(");
+  if (true) {
+    return resolve(true);
+  }
+  return resolve(false);
+});
 
-const createColor = () => {
-  const red = generateColorValue();
-  const green = generateColorValue();
-  const blue = generateColorValue();
-  return `rgb(${red}, ${green}, ${blue})`;
-};
-
-const applyColorToBody = color => {
-  return (document.body.style.backgroundColor = color);
-};
-
-const addRandomColorToBg = () => {
-  const color = createColor();
-  return applyColorToBody(color);
-};
-addRandomColorToBg();
-
-const newColors = document.getElementById("new-colors");
-
-newColors.addEventListener("click", addRandomColorToBg);
-
-// Set interval vs Set Timeout
-// const log = () => console.log('is in timeout');
-// setTimeout(log, 1000);
-
-const interval = setInterval(addRandomColorToBg, 2000);
-newColors.addEventListener("click", () => clearInterval(interval));
+chargeCard()
+  .then((val) => {
+    console.log(val);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
